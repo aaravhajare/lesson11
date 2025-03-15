29627f03b27cd516dc108b08fe5042226849625f
aaravhajare <hajareaarav66@gmail.com>
2025-03-15 22:24:35 +0530

    reversed traingle

---
+++ homework.py
@@ -0,0 +1,13 @@
+n = int(input("enter n.o of rows"))
+
+for i in range(1,n+1) :
+
+    for j in range(1,n + 1):
+
+        if (j <= n-i) :
+          print(" " , end=" ")
+
+        else :
+           print("*" , end=" ")
+
+    print()
